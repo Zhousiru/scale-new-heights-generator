ce131723663f8d5acd9674d441750e745f9a41de
Zhilu <hi@zhilu.cyou>
2026-07-17 07:54:12 +0800

    refactor(utils): 替换cn工具为cnfast
    移除了clsx和tailwind-merge依赖，改用cnfast统一处理类名合并
    
    更新了相关包版本和其他依赖项

--- src/shared/utils/cn.ts
+++ src/shared/utils/cn.ts
@@ -1,6 +1,5 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { cn as cnfast, type ClassValue } from 'cnfast'
 
 export function cn(...inputs: ClassValue[]): string {
-  return twMerge(clsx(inputs))
+  return cnfast(...inputs)
 }
